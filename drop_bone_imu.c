2b5b2af0386de5fc4dc383b2c544fb63b0c326ee
James Ward <j.ward@acfr.usyd.edu.au>
2014-08-08 14:21:09 +1000

    Errors fixed with compile. Return for main(), and min() implemented.

--- drop_bone_imu.c
+++ drop_bone_imu.c
@@ -1,10 +1,13 @@
 #include "drop_bone_imu.h"
 
 #include <stdio.h>
-
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <linux/i2c-dev.h>
 
 int main(int argc, char **argv){
     open_bus();
+    return 0;
 }
 
 int i2c_write(unsigned char slave_addr, unsigned char reg_addr,
@@ -17,12 +20,12 @@ int i2c_read(unsigned char slave_addr, unsigned char reg_addr,
 }
 
 int open_bus() { 
-    if ((fd = open(BBB_I2C_FILE)) < 0) {
+    if ((fd = open(BBB_I2C_FILE, O_RDWR)) < 0) {
         /* ERROR HANDLING: you can check errno to see what went wrong */
         perror("Failed to open the i2c bus");
         return 1;
     }
-    if (ioctl(fd, BBB_I2C_FILE, MPU6050_ADDR) < 0) {
+    if (ioctl(fd, I2C_SLAVE, MPU6050_ADDR) < 0) {
         perror("Failed to acquire bus access and/or talk to slave.\n");
         /* ERROR HANDLING; you can check errno to see what went wrong */
         return 1;
@@ -40,7 +43,7 @@ void reg_int_cb(struct int_param_s *param){
 }
 
 inline int min ( int a, int b ){
-    
+    return a < b ? a : b;
 }
 inline void __no_operation(){
     
